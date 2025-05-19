59b3a12227e94ef9d12122cbf8a152e68ff74d78
Cassie Andrews <candrews1@ufl.edu>
2025-05-19 00:27:41 -0400

    about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,8 +4,10 @@ export default function About() {
   return (
     <div className={styles.about__container}>
         <div>
-            <h1 className={styles.about__header}>About Me</h1>
-            <p className={styles.about}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse totam at in accusantium tempora non nostrum possimus nihil consectetur, quos assumenda consequatur quibusdam inventore necessitatibus temporibus nisi? Accusantium, nesciunt hic.</p>
+            <h1 className={styles.about__header}>Nice to meet you!</h1>
+            <p className={styles.about}>I'm Cassie and I'm currently living in Bradenton, FL. I work as a program assistant at a local arts-based non-profit. I earned my undergraduate degree from Florida State University in book arts and printmaking and I am currently pursuing a Master’s Degree in Web Design & Online Communications at the University of Florida. My background in fine arts has equipped me with a unique perspective as I navigate the ever-evolving realm of digital design.
+            
+            Digital design enables me to create engaging, interactive experiences that parallel the traditional artforms I love like artist books. I am eager to explore balancing art and technology to connect with people in authentic new ways.</p>
         </div>
 
         <div>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,14 +2,14 @@ import styles from '../styles/contact.module.css'
 
 export default function Contact() {
   return (
-    <>
+    <div className="contact__container">
         <h1 className={styles.contact}>Let's talk!</h1>
         <ul>
-            <li>Email:</li>
-            <li>Phone:</li>
-            <li>GitHub:</li>
-            <li>LinkedIn:</li>
+            <li><a href="mailto:candrews1@ufl.edu">Email</a></li>
+            <li><a href="#">Phone</a></li>
+            <li><a href="https://github.com/Cassie-Andrews">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/cassie-andrews/">LinkedIn</a></li>
         </ul>
-    </>
+    </div>
   )
 }
